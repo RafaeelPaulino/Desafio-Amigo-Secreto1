731d4696b423ff6b2153c30c3c2b52d4377ee097
RafaeelPaulino <rafaeel.paulino@gmail.com>
2025-07-31 15:35:19 -0300

    Mexendo no codigo

--- app.js
+++ app.js
@@ -1,1 +1,35 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = [];
+function adicionarAmigo(){
+    const input = document.getElementById("amigo");
+    const nome = input.value.trim();
+
+
+if (nome === ""){
+    alert("Por favor, insira um nome");
+    return;
+}
+amigos.push(nome);
+input.value = "";
+atualizarListaAmigos();
+}
+
+function atualizarListaAmigos() {
+    const lista = document.getElementById("listaAmigos");
+    lista.innerHTML = "";
+
+    for (let i = 0; i < amigos.length; i++) {
+        const item = document.createElement("li");      
+        item.textContent = (`${i + 1} - ${amigos[i]}`);  
+        lista.appendChild(item);                        
+    }
+}
+function sortearAmigo(){ 
+    if (amigos.length === 0){ 
+        alert("A Lista está vazia.");
+    return;
+}
+const indice = Math.floor(Math.random()* amigos.length);
+const sorteado = amigos[indice];
+const resultado = document.getElementById("resultado");
+resultado.innerHTML = (`<li>O amigo sorteado foi: ${sorteado}</li>`);
+}
