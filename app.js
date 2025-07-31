3d9d34b7a2f3f774d195b4a87be673c38cbc2546
RafaeelPaulino <rafaeel.paulino@gmail.com>
2025-07-31 18:07:52 -0300

    Indentar o codigo

--- app.js
+++ app.js
@@ -1,35 +1,34 @@
 let amigos = [];
-function adicionarAmigo(){
-    const input = document.getElementById("amigo");
-    const nome = input.value.trim();
+function adicionarAmigo() {
+  const input = document.getElementById("amigo");
+  const nome = input.value.trim();
 
-
-if (nome === ""){
+  if (nome === "") {
     alert("Por favor, insira um nome");
     return;
-}
-amigos.push(nome);
-input.value = "";
-atualizarListaAmigos();
+  }
+  amigos.push(nome);
+  input.value = "";
+  atualizarListaAmigos();
 }
 
 function atualizarListaAmigos() {
-    const lista = document.getElementById("listaAmigos");
-    lista.innerHTML = "";
+  const lista = document.getElementById("listaAmigos");
+  lista.innerHTML = "";
 
-    for (let i = 0; i < amigos.length; i++) {
-        const item = document.createElement("li");      
-        item.textContent = (`${i + 1} - ${amigos[i]}`);  
-        lista.appendChild(item);                        
-    }
+  for (let i = 0; i < amigos.length; i++) {
+    const item = document.createElement("li");
+    item.textContent = `${i + 1} - ${amigos[i]}`;
+    lista.appendChild(item);
+  }
 }
-function sortearAmigo(){ 
-    if (amigos.length === 0){ 
-        alert("A Lista está vazia.");
+function sortearAmigo() {
+  if (amigos.length === 0) {
+    alert("A Lista está vazia.");
     return;
-}
-const indice = Math.floor(Math.random()* amigos.length);
-const sorteado = amigos[indice];
-const resultado = document.getElementById("resultado");
-resultado.innerHTML = (`<li>O amigo sorteado foi: ${sorteado}</li>`);
+  }
+  const indice = Math.floor(Math.random() * amigos.length);
+  const sorteado = amigos[indice];
+  const resultado = document.getElementById("resultado");
+  resultado.innerHTML = `<li>O amigo sorteado foi: ${sorteado}</li>`;
 }
